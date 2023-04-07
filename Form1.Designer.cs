4459e7eb33aa06beb3de47760f1df40adf2448fe
wojiaoguowei <winner-guowei@163.com>
2023-04-07 09:37:07 +0800

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace WindowsFormsApp12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxX1 = new DevComponents.DotNetBar.Controls.TextBoxX();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(140, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(285, 354);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(571, 137);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 129);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBoxX1
+            // 
+            // 
+            // 
+            // 
+            this.textBoxX1.Border.Class = "TextBoxBorder";
+            this.textBoxX1.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.textBoxX1.Location = new System.Drawing.Point(512, 28);
+            this.textBoxX1.Name = "textBoxX1";
+            this.textBoxX1.PreventEnterBeep = true;
+            this.textBoxX1.Size = new System.Drawing.Size(249, 28);
+            this.textBoxX1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxX1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private DevComponents.DotNetBar.Controls.TextBoxX textBoxX1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp12
+{
+    public partial class Form1 : Form
+    {
+        private Point startPoint;
+        private Rectangle rect = new Rectangle();
+        private bool drawing = false;
+        private Bitmap bitmap;
+        public Form1()
+        {
+            InitializeComponent();
+
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bitmap;
+            pictureBox1.MouseDown += new MouseEventHandler(pictureBox1_MouseDown);
+            pictureBox1.MouseMove += new MouseEventHandler(pictureBox1_MouseMove);
+            pictureBox1.MouseUp += new MouseEventHandler(pictureBox1_MouseUp);
+
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            startPoint = e.Location;
+            drawing = true;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drawing)
+            {
+                // 获取矩形坐标
+                rect.X = Math.Min(startPoint.X, e.X);
+                rect.Y = Math.Min(startPoint.Y, e.Y);
+                rect.Width = Math.Abs(startPoint.X - e.X);
+                rect.Height = Math.Abs(startPoint.Y - e.Y);
+                textBoxX1.Text = String.Format("x:  {0}  y: {1}  width: {2} height: {3}", rect.X, rect.Y, rect.Width, rect.Height);
+                // 将画布重置为原始图片
+                bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                pictureBox1.Image = bitmap;
+
+                // 在画布上绘制矩形
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.DrawRectangle(Pens.Red, rect);
+                }
+
+                pictureBox1.Invalidate();
+            }
+        }
+
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            drawing = false;
+
+            // Get the coordinates of the rectangle
+            int x = rect.X;
+            int y = rect.Y;
+            int width = rect.Width;
+            int height = rect.Height;
+
+            // Create a new image with the same dimensions as the rectangle
+            Bitmap croppedImage = new Bitmap(new Bitmap(@"D:\work\develop\杨铭交接\MAX-100上位机交接资料\右侧模块图片\right\MAX100-XT-AI2 AO1.png"), width, height);
+            // Copy the contents of the rectangle from the original image to the new image
+            using (Graphics g = Graphics.FromImage(croppedImage))
+            {
+                g.DrawImage(pictureBox1.Image, new Rectangle(0, 0, width, height), new Rectangle(x, y, width, height), GraphicsUnit.Pixel);
+            }
+
+            // Set the new image as the image of a PictureBox
+            PictureBox newPictureBox = new PictureBox();
+            newPictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            newPictureBox.Location = new Point(x, y);
+            newPictureBox.Size = new Size(width, height);
+            newPictureBox.Image = croppedImage;
+
+            // Add the new PictureBox to the form
+            pictureBox1.Controls.Add(newPictureBox);
+        }
+    }
+}
